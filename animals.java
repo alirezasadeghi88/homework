21c6d110802c0a501a806b04ec8e02b5b6c07548
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-10-02 10:45:39 -0700

    first commit

---
+++ animals.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Animals{
+	public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+
+		System.out.print("name");
+		String name = input.next();
+
+		System.out.print("size");
+		int size = input.nextInt();
+
+		System.out.print("type");
+		String type = input.next();
+
+		System.out.print("weight");
+		double weight = input.nextDouble();
+
+		System.out.println(name);
+
+	}
+}
