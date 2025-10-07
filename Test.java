28e2a22608e834946ee484aa56e5bec18535a329
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-10-07 10:04:47 -0700

    R

--- Test.java
+++ Test.java
@@ -1,4 +1,5 @@
 //import java.lang.String;
+//import java.lang.System;
 import java.util.Scanner;
 
 public class Test {
--- Test2.java
+++ Test2.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 public class Test2 {
 	
 	public static void main(String[] args){
-		System.out.print("Test");
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("--------------------");
+		System.out.println("---- Calculator ----");
+		System.out.println("--------------------");
+		System.out.print("Enter your number1: ");
+		int number1 = input.nextInt();
+		System.out.print("Enter your Opteration: ");
+		String opt = input.next();
+		System.out.print("Enter your number2: ");
+		int number2 = input.nextInt();
+		
+		if(opt.equals("+")){
+			System.out.println(number1 + number2);
+		} else if (opt.equals("-")){
+			System.out.println(number1 - number2);
+		} else if (opt.equals("*")){
+			System.out.println(number1 * number2);
+		} else if (opt.equals("/")){
+			System.out.println(number1 / number2);
+		} else if (opt.equals("%")){
+			System.out.println(number1 % number2);
+		}	
 	}
 }
