544dd5b8ca52d323c7484fdd352d7b65167c1d5d
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-10-09 00:56:37 -0700

    R

---
+++ Calculator.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Calculator {
+	
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("----------------");
+		System.out.println(" --CALCULATOR-- ");
+		System.out.println("----------------");
+		System.out.print("Enter your number1= ");
+		int number1 = input.nextInt();
+		System.out.print("Enter your Opteration= ");
+		String opt = input.next();
+		System.out.println("Enter your number2= ");
+		int number2 = input.nextInt();
+		
+		if(opt.equals("+")){
+			System.out.println(number1 + number2);
+		} else if(opt.equals("-")){
+			System.out.println(number1 - number2);
+		} else if(opt.equals("*")){
+			System.out.println(number1 * number2);
+		} else if(opt.equals("/")){	
+			System.out.println(number1 / number2);
+		} else if(opt.equals("%")){
+			System.out.println(number1 % number2);
+		}
+	}	
+}		
+		
